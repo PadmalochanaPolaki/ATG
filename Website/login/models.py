78572007263a5953e513daba5a6ab8ef782370b7
PadmalochanaPolaki <140701129+PadmalochanaPolaki@users.noreply.github.com>
2023-11-30 17:40:09 +0530

    Delete Website directory

--- Website/login/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class data(models.Model):
-    Fname = models.CharField(max_length=50)
-    Lname = models.CharField(max_length=50)
-    Username = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    password= models.CharField(max_length=50)
-    Address= models.CharField(max_length=60)
